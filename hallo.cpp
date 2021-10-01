c04d35bbad80f2b2a36e9d911dfe7a9051680793
Jakob Klausbauer <jakob.klausbauer@bulme.at>
2021-10-01 12:50:03 +0200

    Added hallo.cpp

---
+++ hallo.cpp
@@ -0,0 +1,14 @@
+//mein hallo-programm
+
+#include <iostream>
+#define verwende using
+#define Namensbereich namespace
+#define standard std
+
+verwende Namensbereich standard;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
